9810942bc83fdbe24968474d77397a76c7365ad5
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-11 09:06:08 +0200

    add fileUploader in registerForm

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -14,10 +14,12 @@ import { useRouter } from "next/navigation";
 import { createUser } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Doctors, GenderOptions } from "@/constants";
+import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
 import { Label } from "../ui/label";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
+import { FileUploader } from "../FileUploader";
+
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
@@ -227,6 +229,60 @@ const RegisterForm = ({ user }: { user: User }) => {
             placeholder="Antibiotic 1mg "
           />
         </div>
+        <div className="flex flex-col gap-6 xl:flex-row text-white">
+          <CustomFormField
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="familyMedicalHistory"
+            label="Family medical history"
+            placeholder="Father had heart disease"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="pastMedicalHistory"
+            label="Past medical history"
+            placeholder="Appendectomy"
+          />
+        </div>
+
+        <section className="space-y-6 text-white">
+          <div className="mb-9 space-y-1"></div>
+          <h2 className="sub-header">Identification and Verification</h2>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.SELECT}
+          control={form.control}
+          name="identificationType"
+          label="Identification type"
+          placeholder="Select an identification type"
+        >
+          {IdentificationTypes.map((type) => (
+            <SelectItem key={type} value={type} className="cursor-pointer">
+              {type}
+            </SelectItem>
+          ))}
+        </CustomFormField>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="identificationNumber"
+          label="Identification number"
+          placeholder="123456789"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.SKELETON}
+          control={form.control}
+          name="identificationDocument"
+          label="Scanned copy of identification document"
+          renderSkeleton={(field) => (
+            <FormControl>
+              <FileUploader files={field.value} onChange={field.onChange} />
+            </FormControl>
+          )}
+        />
 
         <div className="flex flex-col gap-6 xl:flex-row"></div>
 
