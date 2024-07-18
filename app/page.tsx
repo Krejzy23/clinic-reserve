5d7aa259191bca9107873952c68408b5e7ef14ab
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-18 22:29:56 +0200

    add admin setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,32 @@
-import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+
+import { PasskeyModal } from "@/components/PasskeyModal";
+import PatientForm from "@/components/forms/PatientForm";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
-    <div className="flex h-screen max-h-screen text-white">
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
-            width={1000} 
+            width={1000}
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
-          {/* <PatientForm /> */}
+
+          <PatientForm />
+
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 ClinicReserve
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
@@ -25,6 +34,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
@@ -34,4 +44,6 @@ export default function Home() {
       />
     </div>
   );
-}
+};
+
+export default Home;
