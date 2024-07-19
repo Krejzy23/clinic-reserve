ff9dba7bc0ec27db5353a934049177dbcafb5ba0
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-19 12:16:02 +0200

    add dropdown menu on data table

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,15 @@
+import { DataTable } from '@/components/table/DataTable'
 import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { columns } from '@/components/table/columns'
 
-const Admin = () => {
+const Admin = async () => {
+
+  const appointments = await getRecentAppointmentList()
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -19,7 +25,7 @@ const Admin = () => {
         </Link>
       </header>
 
-      <main className="">
+      <main className="admin-main">
         <section className="w-full space-y-4">
           <h1 className="header">Welcome 👋</h1>
           <p className="text-dark-700">Start the day with managing new appointments.</p>
@@ -28,23 +34,25 @@ const Admin = () => {
         <section className='admin-stat'>
           <StatCard 
             type="appointments"
-            count={5}
+            count={appointments.scheduledCount}
             label="Schedule appointment"
             icon="/assets/icons/appointments.svg"
           />
           <StatCard 
             type="pending"
-            count={10}
+            count={appointments.pendingCount}
             label="Pending appointment"
             icon="/assets/icons/pending.svg"
           />
           <StatCard 
             type="cancelled"
-            count={2}
+            count={appointments.cancelledCount}
             label="Cancelled appointment"
             icon="/assets/icons/cancelled.svg"
           />
         </section>
+
+        <DataTable columns={columns} data={appointments.documents}/>
       </main>
     </div>
   )
