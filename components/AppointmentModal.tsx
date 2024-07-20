6178ca6614d27b63f20968fc7d1cc966f1b8aab7
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-20 13:43:57 +0200

    add cancel schedule

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -1,29 +1,63 @@
-import React from "react";
+"use client";
 
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"  
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Appointment } from "@/types/appwrite.types";
 
-const AppointmentModal = () => {
+import { AppointmentForm } from "./forms/AppointmentForm";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export const AppointmentModal = ({
+  patientId,
+  userId,
+  appointment,
+  type,
+}: {
+  patientId: string;
+  userId: string;
+  appointment?: Appointment;
+  type: "schedule" | "cancel";
+  title: string;
+  description: string;
+}) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={`capitalize ${type === "schedule" && "text-green-500"}`}
+        >
+          {type}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-3">
+          <DialogTitle className="capitalize">{type} Appointment</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Please fill in the following details to {type} appointment
           </DialogDescription>
         </DialogHeader>
+
+        <AppointmentForm
+          userId={userId}
+          patientId={patientId}
+          type={type}
+          appointment={appointment}
+          setOpen={setOpen}
+        />
       </DialogContent>
     </Dialog>
   );
 };
-
-export default AppointmentModal;
